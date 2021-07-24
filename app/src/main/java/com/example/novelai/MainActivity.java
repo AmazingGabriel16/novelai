d5a0f97e46688bf919db75a97a27441d832a5ffd
Gabriel Tobing <6481213l@gmail.com>
2021-07-24 20:56:19 +1000

    Version 1.0.1 - Preview APK
    This is a preview APK in an attempt to fix the download/import support. There are still many things that need to be worked on, as the APK will crash when somebody tries to download something from the Novel AI website, but it can for some reason download PDFs, appears to be a file type handling problem, but I am unsure.
    
    Never the less, this is the current progress of things.
    
    Changelogs
    
    Version 1.0.1
    
    + Added hardware acceleration on the application level.
    + Added external storage, read and write permissions for the download and export of stories.
    
    - Removed unsued colour values in the file "colors.xml". We are only using the "nai_theme" as the default colour scheme.
    
    Issues
    
    - The program is able to downlaod files such as PDFs via the webview element, but will crash when you click on the downlaod button for the stories inside of the Novel AI website. This may be due to the file type. Tested on the Pixel 3A API 30 x86.
    
    Credit: Sabith Pkc Mnr
    (https://www.youtube.com/watch?v=n-iXcR7MHgw)

--- app/src/main/java/com/example/novelai/MainActivity.java
+++ app/src/main/java/com/example/novelai/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.novelai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
@@ -26,8 +30,24 @@ public class MainActivity extends AppCompatActivity {
         mywebView.setWebViewClient(new WebViewClient());
         mywebView.getSettings().setDomStorageEnabled(true);
         mywebView.loadUrl("https://www.novelai.net");
+        //mywebView.loadUrl("https://www.google.com");
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        // Attempt at the download stuff
+        mywebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                DownloadManager.Request myRequest = new DownloadManager.Request(Uri.parse(url));
+                myRequest.allowScanningByMediaScanner();
+                myRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Toast.makeText(MainActivity.this, "SECTOR 1", Toast.LENGTH_SHORT).show();
+                DownloadManager myManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                myManager.enqueue(myRequest);
+
+                Toast.makeText(MainActivity.this, "Story downloading...", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public class mywebClient extends WebViewClient{
@@ -36,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             super.onPageStarted(view,url,favicon);
         }
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view,String url){
+        public boolean shouldOverrideUrlLoading(WebView view,String url) {
             view.loadUrl(url);
             return true;
         }
